a930cbe533d14dff96dcd352bb7ac97e06dc5fdf
manishkumar632 <manishmu632@gmail.com>
2024-05-22 12:41:15 +0530

    Question 1 Solution

---
+++ Exercises_Solutions/Question1.cpp
@@ -0,0 +1,3 @@
+// Write a program that declares variables of each basic data type (int, float, double, char, and bool), assigns them values, and prints these values to the console.
+
+#include <iostream>
