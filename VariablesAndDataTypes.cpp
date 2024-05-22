0f1c80d0eebf52971bdd47073df2a4cd9497b84b
manishkumar632 <manishmu632@gmail.com>
2024-05-22 12:37:01 +0530

    Variables and Data Types in C++

---
+++ VariablesAndDataTypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main(){
+    int a = 10;
+    float b = 10.5;
+    char c = 'a';
+    bool d = true;
+    double e = 10.5324; // Double is more precise than float.
+    std::cout << "integer a : " << a << std::endl;
+    std::cout << "float b : " << b << std::endl;
+    std::cout << "char c : " << c << std::endl;
+    std::cout << "bool d : " << d << std::endl;
+    std::cout << "double e : " << e << std::endl;
+    return 0;
+}
